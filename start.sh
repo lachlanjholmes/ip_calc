3c589195cf5381b9d4a45f19564a87081d1003aa
lachlanjholmes <1935116+lachlanjholmes@users.noreply.github.com>
2025-07-04 13:30:01 +1000

    added dev-start.sh for local testing

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONTAINER_CLI="podman"
+if ! command -v $CONTAINER_CLI &> /dev/null
+then
+    CONTAINER_CLI="docker"
+    if ! command -v $CONTAINER_CLI &> /dev/null
+    then
+        echo "Error: Neither Podman nor Docker is installed. Please install one of them to run the application."
+        exit 1
+    fi
+fi
+
+$CONTAINER_CLI rm -f ip_calc
+$CONTAINER_CLI build -t ip_calc .
+$CONTAINER_CLI run -d -p 8085:80 --name ip_calc ip_calc
